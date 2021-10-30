051ebf9f01470bdceaec7ae524885cb2d9d4ab4d
marmot0814 <s1357andy871207@gmail.com>
2021-10-31 01:39:01 +0800

    deploy

--- src/data/const.js
+++
@@ -1,2 +0,0 @@
-export const STU_NAME = '王大明';
-export const STU_ID = '108000001';
--- src/data/problems/20160305.js
+++
@@ -1,8 +0,0 @@
-import p1 from './20160305-p1.pdf';
-import p2 from './20160305-p2.pdf';
-import p3 from './20160305-p3.pdf';
-import p4 from './20160305-p4.pdf';
-
-const problems = { p1, p2, p3, p4 };
-
-export default problems;
